89f782bc0522ad08107e5a1a3b427d9ac05950c5
Francisco Messina <91835899+FranciscoMessina00@users.noreply.github.com>
2025-01-26 21:50:40 +0100

    Add files via upload

---
+++ apiData.py
@@ -0,0 +1,5 @@
+azure_endpoint="https://aui-10a.openai.azure.com/"
+api_key="8QKWZTPFiFGKpNDjGcnQLuKFODRgb8pdcdrYdTcxFEm8poKZL16iJQQJ99AKACfhMk5XJ3w3AAABACOG5QSm"
+
+azure_image_endpoint = "https://aui-10a.openai.azure.com/"
+api_image_key = "8QKWZTPFiFGKpNDjGcnQLuKFODRgb8pdcdrYdTcxFEm8poKZL16iJQQJ99AKACfhMk5XJ3w3AAABACOG5QSm"
